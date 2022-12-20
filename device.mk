42b494af1eadc20d56e387080a06b54c6beb5100
spkal01 <kalligeross@gmail.com>
2022-12-20 07:21:01 +0200

    derps will rebase  later

--- device.mk
+++ device.mk
@@ -53,16 +53,5 @@ PRODUCT_PACKAGES += \
     libcodec2_soft_common.vendor \
     libsfplugin_ccodec_utils.vendor
 
-
-# Perf
-PRODUCT_PACKAGES += \
-    libpsi.vendor \
-    libtflite
-
-# Perf Jars
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
-
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
